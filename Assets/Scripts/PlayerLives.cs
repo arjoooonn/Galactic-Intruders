0c5dda6c19a3792d7c44ace7e5ba100d16d8e0ef
Arjun <arjooon05@gmail.com>
2025-05-07 04:31:17 +0530

    The Whole Project
    In this i have pushed the whole unity project to github

---
+++ Assets/Scripts/PlayerLives.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerLives : MonoBehaviour
+{
+    public int lives = 3;
+    public Image[] livesUI;
+    public GameObject explosionPrefab;
+
+    private void OnTriggerEnter2D(Collider2D other) // Changed parameter type only
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            Destroy(other.gameObject); // Changed from collision.collider to other
+            Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+            lives -= 1;
+            for (int i = 0; i < livesUI.Length; i++)
+            {
+                if (i < lives)
+                {
+                    livesUI[i].enabled = true;
+                }
+                else
+                {
+                    livesUI[i].enabled = false;
+                }
+            }
+            if (lives <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -6,10 +6,12 @@ public class Projectile : MonoBehaviour
 {
     public float moveSpeed;
     public GameObject explosionPrefab;
+    private PointManager pointManager;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        pointManager = GameObject.Find("PointManager").GetComponent<PointManager>();
     }
 
     // Update is called once per frame
@@ -24,6 +26,7 @@ public class Projectile : MonoBehaviour
         {
             Instantiate(explosionPrefab, transform.position, Quaternion.identity);
             Destroy(collision.gameObject);
+            pointManager.UpdateScore(50);
             Destroy(gameObject);
         }
         if (collision.gameObject.tag == "Boundary")
